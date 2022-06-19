b3be06d352b65f46532bdf37d62f201f01ab0e7e
mbayu180 <mbayu180@gmail.com>
2022-06-19 21:49:48 +0700

    KONFIGURASI JS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import "./Contact.css";
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
-    )
-}
+      <div className='Contact'>
+        <img className="foto" src={data.photo} alt={data.name}></img>
+      <section className="Detail">
+        <h3>{data.name}</h3>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </section>
 
-export default Contact;
+      </div>
+    );
+};
+
+export default Contact; 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
+import "./Header.css";
 import React from 'react';
 
 const Header = () => {
     return (
-        <></>
-    )
-}
 
-export default Header;
+        <div className='Header'>
+        <h1> Call A Friend</h1>
+        <p>Your Friendly Contact app</p>
+        <p>
+            ***************************************************************************
+        </p>
+        </div>
+    );
+};
+
+export default Header; 
